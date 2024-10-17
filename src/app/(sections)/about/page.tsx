77c42b22920a79005eceb255ec7fb339d23ed0c3
sebas1913 <sebasosornicastrillon@gmail.com>
2024-10-17 08:41:29 -0500

    Create component experience

--- src/app/(sections)/about/page.tsx
+++ src/app/(sections)/about/page.tsx
@@ -8,7 +8,7 @@ export default function About() {
 
         <>
             <ProfileCard />
-            {/* <Experience /> */}
+            <Experience />
             <Technologies />
             <AboutMe />
         </>
