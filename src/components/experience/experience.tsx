00197a472278b1a5a746004fe8ff6d1f87426742
sebas1913 <sebasosornicastrillon@gmail.com>
2024-10-18 06:40:54 -0500

    Responsive and bug fixes

--- src/components/experience/experience.tsx
+++ src/components/experience/experience.tsx
@@ -1,12 +1,15 @@
 import React from "react";
-import styles from './experience.module.scss';
+// import styles from './experience.module.scss';
 
 
 const Experience: React.FC = () =>{
     return(
-        <div className={styles.container}>
-            <h1 className={styles.title}>Experiencia</h1>
-        </div>
+        // <div className={styles.container}>
+        //     <h1 className={styles.title}>Experiencia</h1>
+        // </div>
+        <>
+
+        </>
     )
 }
 
